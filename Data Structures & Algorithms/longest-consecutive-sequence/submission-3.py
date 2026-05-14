680b4a0c8df224585afc558b256d6d452f502e75
Piyush Pokharkar <66646406+p-rp@users.noreply.github.com>
2026-05-14 13:35:21 -0700

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # num : seq_len
+        seen = defaultdict(int)
+
+        longest_seq = 0
+
+        for num in nums:
+
+            if seen[num]  == 0:
+                seen[num] += seen[num-1] + seen[num+1] + 1
+
+                seen[num - seen[num-1]] = seen[num]
+                seen[num + seen[num+1]] = seen[num]
+
+                longest_seq = max(longest_seq, seen[num])
+
+        return longest_seq
+
+
+"""
+max = 0 -> 1
+seen {2:3, 20:1, 4:3, 10:1, 3:3, 5:4}
+"""
